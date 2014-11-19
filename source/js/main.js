bef41b9c2cd245350397e223cf11632a81c1953f
Josh Franklin <joshfranklin@Joshs-MacBook-Pro.local>
2014-11-18 19:48:27 -0500

    added about section

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 $('a[href*=#]').click(function(event){
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 800);
+    }, 500);
     event.preventDefault();
 });
