c3a0fbc88a36117ef0f8b24d9851a7f3702e5685
Josh Franklin <joshfranklin@Joshs-MBP.home>
2014-11-23 22:10:27 -0500

    adjustments to mobile nav and header animation

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,14 @@
-$('a[href*=#]').click(function(event){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    event.preventDefault();
+$( document ).ready(function() {
+
+	// $('a[href*=#]').click(function(event){
+	// 	$('html, body').animate({
+	// 	    scrollTop: $( $.attr(this, 'href') ).offset().top
+	// 	}, 500);
+	// 	event.preventDefault();
+	// });
+
+	$(".mobile-btn").click(function(){
+	    $("ul").toggleClass("lista");
+	});
+    
 });
